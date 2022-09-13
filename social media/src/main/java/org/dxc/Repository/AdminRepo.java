80883f0e56f19f6756b6d613a9385c37e331f036
Lee Jun Hao <mastehaoleekuma@gmail.com>
2022-09-13 11:52:16 +0800

    added base file

---
+++ social media/src/main/java/org/dxc/Repository/AdminRepo.java
@@ -0,0 +1,10 @@
+package org.dxc.Repository;
+
+import org.dxc.entity.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AdminRepo extends JpaRepository<Admin,Long> {
+
+}
---
+++ social media/src/main/java/org/dxc/entity/Admin.java
@@ -0,0 +1,32 @@
+package org.dxc.entity;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table
+public class Admin  {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id; 
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	private String userName;
+	private String password;
+}
